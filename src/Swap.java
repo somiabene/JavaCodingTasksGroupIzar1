c8ff28f940d4fd0a1b2d2cbc17b89b257f081032
lobdob <elmirabayramova786@gmail.com>
2023-06-01 14:56:32 -0400

    added Swap, DivisibleBy, Consecutive Numbers

---
+++ src/Swap.java
@@ -0,0 +1,29 @@
+public class Swap {
+    public static void main(String[] args) {
+
+        int a = 19;
+        int b = 10;
+
+        a = a + b; // 19+10=29
+
+        b = a - b; // 29-10=19
+
+        a = a - b; // 29-19=10
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+    }
+
+}
+
+
+/*
+
+. Swap Numbers
+  Swap two variable' values without using a third variable.'
+
+
+*/
